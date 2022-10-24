a2d8c69356bf4bf12208b525dfa3cede98705a94
Luis Martin <luis.martinf89@gmail.com>
2022-10-24 15:43:06 -0500

    creacion de imagenes webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//SCSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+//IMAGES
+const webp = require('gulp-webp');
+
 
 function css(done){
     src('src/scss/**/*.scss') // Identificar el archivo de SASS
@@ -10,10 +16,23 @@ function css(done){
     done(); // Callback que avisa a gulp cuando llegamos al final de la funcion
 }
 
+function versionWebp(done){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css)
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp; 
+exports.dev = parallel(versionWebp, dev);
