1898b6e5523e46dc92a71495f072b223906dff7d
Luis Martin <luis.martinf89@gmail.com>
2022-10-27 12:05:50 -0500

    imagenes avif

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 
 function css(done){
@@ -29,13 +30,21 @@ function imagenes(done){
 }
 
 function versionWebp(done){
-
     const opciones = {
         quality: 50
     };
-
     src('src/img/**/*.{png,jpg}')
         .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
         .pipe(dest('build/img'))
     done();
 }
@@ -47,5 +56,6 @@ function dev(done){
 
 exports.css = css;
 exports.imagenes= imagenes;
-exports.versionWebp = versionWebp; 
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif; 
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
